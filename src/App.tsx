5079ad5e52486c698da82a685d36d644b5939fe2
revapoddar <revap@andrew.cmu.edu>
2025-03-13 11:20:49 -0400

    newfiletypefix

--- src/App.tsx
+++ src/App.tsx
@@ -121,16 +121,19 @@ function App() {
     setMessage("");
 
     try {
-      const formData = new FormData();
-      formData.append("file", file);
+      // const formData = new FormData();
+      // formData.append("file", file);
+
+      // Read file as ArrayBuffer (raw binary)
+      const fileBuffer = await file.arrayBuffer();
 
       const response = await fetch(API_URL, {
         method: "POST",
         headers: {
-          //"Content-Type": "application/pdf",
+          "Content-Type": "application/pdf",
           "x-api-key": "CAaJOxCLmS9S8vwiI1d3s9JnVJmJ6Z6V4oqymjdx",
         },
-        body: formData,
+        body: fileBuffer,
         mode: "cors",
       });
 
